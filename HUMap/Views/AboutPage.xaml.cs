2ffc3ca325513f9799a927cbd54113dfbc0f48a8
Cormac McGreevy <C.D.MCGREEVY-2021@hull.ac.uk>
2023-11-28 10:37:01 +0000

    Revert "update to prep for future"
    This reverts commit 3a2e0ccfa6c69874b59f907078984aaef93961f7.

---
+++ HUMap/Views/AboutPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace HUMap.Views;
+
+public sealed partial class AboutPage
+{
+    private AboutViewModel _vm;
+
+    public AboutPage(AboutViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+        _vm = viewModel;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        var versionLabel = this.FindByName<Label>("version");
+        if (versionLabel != null)
+            versionLabel.Text = $"v{VersionTracking.CurrentVersion}";
+    }
+}
